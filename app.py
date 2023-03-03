eba0bd69c98465f722f83dccdc1a4f88059f8925
DM-GSWL <126910487+DM-GSWL@users.noreply.github.com>
2023-03-03 13:41:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,11 @@ from cgi import test
 import streamlit as st
 
 st.set_page_config(page_title="test web page", layout="wide")
-st.changeUrl("test")
+
 st.header("what is up GSWL")
+response = openai.Image.create(
+  prompt="a white siamese cat",
+  n=1,
+  size="1024x1024"
+)
+image_url = response['data'][0]['url']
